4029781b7fb84d790681e8d6e5aa6807362fa6c4
Hal D <hal.dimarchi55@gmail.com>
2021-06-01 12:40:30 -0700

    created cleaner install method

--- install.sh
+++ install.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-echo 'Installing conda-pack'
-conda install conda-pack
-
-echo 'Installing composite wrapper environment to ./comp_wrapper_env'
-mkdir -p comp_wrapper_env
-tar -xzf comp_wrapper_env.tar.gz -C comp_wrapper_env
-
-echo 'exit' || ./comp_wrapper_env/bin/python
-
-source ./comp_wrapper_env/bin/activate
-
-echo 'exit' || python
-conda-unpack
-
-echo 'Environment installed.'
+conda create -y --name composite_wrapper_env -c conda-forge --file env.txt
