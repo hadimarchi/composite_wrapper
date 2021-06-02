98f168021765f199843fe966c3789ead1c853b46
Hal D <hal.dimarchi55@gmail.com>
2021-06-02 13:03:44 -0700

    modified run.sh to use conda env properly

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source ./comp_wrapper_env/bin/activate
+conda activate composite_wrapper_env
 python run.py --username username --password password --granule_count 5 --lon_lat_minimum 150.0 65.0 --lon_lat_maximum 150.2 65.5
