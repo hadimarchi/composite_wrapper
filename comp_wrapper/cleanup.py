0d7b9de3c24277ce7ac011c2d3fe7b097f2cd6ef
Hal D <hal.dimarchi55@gmail.com>
2021-07-13 12:33:29 -0700

    Removed conflicting add_argument settings for cleanup flag and wrote dummy code in cleanup.py

--- comp_wrapper/cleanup.py
+++ comp_wrapper/cleanup.py
@@ -1,1 +1,4 @@
-# For the cleaning of SLC or GRD granules after 
+# For the cleaning of SLC or GRD granules after running composite
+
+def cleanup_granules():
+    return None
--- comp_wrapper/cmd_parser.py
+++ comp_wrapper/cmd_parser.py
@@ -8,7 +8,6 @@ composite_options_parser.add_argument('--granule_count', type=int, default=10, h
 composite_options_parser.add_argument('--lon_lat_minimum', type=float, nargs="+", help="coordinate to define lower left corner of bounding box")
 composite_options_parser.add_argument('--lon_lat_maximum', type=float, nargs="+", help="coordinate to define upper right corner of bounding box")
 composite_options_parser.add_argument('--polarization', type=str, choices=["vv", "vh", "vv+vh"], default="vv+vh", help="polarization of granules")
-composite_options_parser.add_argument('--cleanup', type=str, choices=["y", "n"], default="n", help="remove original granules")
 composite_options_parser.add_argument('--start_date', type=str, help="start date for granule search in the format yyyy-mm-dd")
 composite_options_parser.add_argument('--end_date', type=str, help="end date for granule search in the format yyyy-mm-dd")
 composite_options_parser.add_argument('--verbose', action='store_true', help="if this flag is added the logging level will be set at INFO")
