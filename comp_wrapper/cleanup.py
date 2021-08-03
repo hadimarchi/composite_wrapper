4e8afe84a35169399d412ca2b24c6b1d93fafc6a
Hal D <hal.dimarchi55@gmail.com>
2021-08-03 11:24:07 -0700

    working cleanup module

--- comp_wrapper/cleanup.py
+++ comp_wrapper/cleanup.py
@@ -12,24 +12,26 @@ def cleanup_gamma_products(download_location):
 
 def remove_zipped_gamma_products(download_location):
     logger.debug("Removing zipped gamma products")
-    zipped_gamma_products = Path(download_location).rglob("*.zip")
-    zipped_gamma_products.unlink()
+    zipped_gamma_products = sorted(Path(download_location).rglob("*.zip"))
+    for product in zipped_gamma_products:
+        product.unlink()
 
 
 def remove_unzipped_gamma_products(download_location):
     logger.debug("Removing unzipped gamma product files")
-    unzipped_gamma_products_files = Path(download_location).glob("S*/*")
+    unzipped_gamma_products_files = sorted(Path(download_location).glob("S*/*"))
     logger.debug("{file_count} gamma product files will be deleted".format(
         file_count=len(list(unzipped_gamma_products_files)))
         )
-    unzipped_gamma_products_files.unlink()
+    for file in unzipped_gamma_products_files:
+        file.unlink()
 
     logger.debug("Removing empty gamma product directories")
-    unzipped_gamma_product_directories = Path.glob("S*")
+    unzipped_gamma_product_directories = sorted(Path(download_location).glob("S*"))
     logger.debug("Found {directory_count} directories or files matching 'S*' pattern to be deleted".format(
         directory_count=len(list(unzipped_gamma_product_directories))
         ))
-    for gamma_product_directory in unzipped_gamma_product_directories.iterdir():
+    for gamma_product_directory in unzipped_gamma_product_directories:
         if not gamma_product_directory.is_dir():
             logger.warning("Unexpected file in gammma products path")
         else:
